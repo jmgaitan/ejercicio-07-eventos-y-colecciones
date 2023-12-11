6c81fcc43d4aa8f80a5035a60ad6a4edff49d412
juanMa Gaitan <jmgaitan93@gmail.com>
2023-12-11 07:39:20 -0300

    punto 7

--- js/main.js
+++ js/main.js
@@ -1,18 +1,67 @@
-document.addEventListener('DOMContentLoaded',function(){
-    //punto 2
-    console.log('Contenido del DOM cargado')
+document.addEventListener('DOMContentLoaded', function() {
+ 
+    function repeatContent(times) {
+    const content = textarea.value;
+        let newContent = '';
+        for(let i = 0; i < times; i++) {
+            newContent += i+1 + ')' + content + '<br>';
+        }
+        return newContent;
+    }
 
-    const contenidoInicial = 'Este contenido está listo para ser editado y pasarlo abajo.';
-    document.getElementById('origen').value = contenidoInicial;
+    // punto 1: cargar el dom e informar por consola
+    console.log("Contenido del DOM cargado");
 
-    document.getElementById('origen').addEventListener('input', () => {
-        document.querySelectorAll('input[type="button"], button').forEach(btn => btn.disabled = false);
+    // punto 2: cargar textarea
+    const textarea = document.getElementById('origen');
+    textarea.value = 'Este contenido está listo\npara ser editado y pasarlo abajo.';
+
+
+    textarea.addEventListener('input', function() {
+        document.querySelectorAll('input[type=button], button').forEach(function(button) {
+            button.disabled = false;
+        });
     });
 
-    const aplicarHTML = (texto) => {
-        return '<p>' + texto.replace('está listo', '<strong>está listo</strong>').replace('para ser editado y pasarlo abajo.', 'para ser editado y pasarlo abajo.<br>') + '</p>';
-    };
+    // funcion de repetir contenido
 
+    document.getElementById('btn-reemplazar').addEventListener('click', function() {
+        document.getElementById('destino').innerHTML = textarea.value;
+    });
 
+    document.querySelectorAll('.btn-agregar').forEach(function(button) {
+        button.addEventListener('click', function() {
+
+            if(button.value === "Agregar n veces") {
+                const n = parseInt(prompt("¿Cuántas veces quieres agregar el contenido?"), 10);
+                if(n > 0) {
+                    document.getElementById('destino').innerHTML += repeatContent(n);
+                }
+            } else {
+                const times = parseInt(button.value.match(/\d+/), 10) || 1;
+                document.getElementById('destino').innerHTML += repeatContent(times);
+            }
+        });
+    });
     
-})
+
+    document.querySelector('.btn-vaciar').addEventListener('click', function() {
+        document.getElementById('destino').innerHTML = '';
+    });
+
+    document.querySelector('.btn-convertir-a-mayusculas').addEventListener('click', function() {
+        const destino = document.getElementById('destino');
+        destino.innerHTML = destino.innerHTML.toUpperCase();
+    });
+
+    document.querySelector('button').addEventListener('click', function() {
+        const destino = document.getElementById('destino');
+        destino.innerHTML = destino.innerHTML.toLowerCase();
+    });
+
+    // punto 10 [Ok]
+    const listItems = document.querySelectorAll('li');
+    listItems.forEach(function(li) {
+        li.textContent = "[Ok] " + li.textContent;
+    });
+});
